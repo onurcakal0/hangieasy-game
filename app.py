99a494b854abb98c4e065fedd88a594da69da2e4
Mustafa Onur Çakal <onur@Onur-MacBook-Air.local>
2026-06-26 01:15:36 +0300

    Add sitemap.xml to root via route

--- app.py
+++ app.py
@@ -1732,6 +1732,10 @@ def hangisi_tepki_ver():
 def serve_ads_txt():
     return send_from_directory('static', 'ads.txt')
 
+@app.route('/sitemap.xml')
+def serve_sitemap_xml():
+    return send_from_directory('static', 'sitemap.xml')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
     socketio.run(app, host='0.0.0.0', port=port, debug=True)
